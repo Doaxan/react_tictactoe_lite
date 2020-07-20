709027eae05acd4c51901c1212c9a7020540a4ea
Amir Fazleev <doaxan77@gmail.com>
2020-07-20 23:57:20 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+      count: 0,
+    }
+    this.winnerLine = [
+      // horizontal
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      // vertical
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      // diagonal
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+  }
+
+  isWinner = (current) => {
+    const currentSquares = this.state.squares
+    this.winnerLine.forEach(line => {
+      if (currentSquares[line[0]] === current && currentSquares[line[1]] ===
+        current && currentSquares[line[2]] === current) {
+        alert(current + ' win!')
+        setTimeout(() => {
+          this.setState({
+            squares: Array(9).fill(null),
+            count: 0,
+          })
+        }, 3000)
+      }
+    })
+  }
+
+  clickHandler = event => {
+    const data = event.target.getAttribute('data')
+    const currentSquares = this.state.squares
+    if (!currentSquares[data]) {
+      currentSquares[data] = this.state.count % 2 === 0 ? 'X' : 'O'
+      this.setState({squares: currentSquares, count: this.state.count + 1})
+    } else {
+      alert('You can not do it this way')
+    }
+    this.isWinner(currentSquares[data])
+  }
+
+  render() {
+    return (
+      <div className="tic-tac-toe">
+        {this.state.squares.map((square, index) =>
+          <div className="ttt-grid"
+               onClick={this.clickHandler} data={index} key={index}>{square}
+          </div>,
+        )}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App title="My site"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
